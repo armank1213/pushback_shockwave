ff33b54dbb496f4b1907f6dd9ee4497a7873f393
Achintyan Kaliraj <achintyan.kaliraj@gmail.com>
2025-12-13 03:31:04 -0800

    Refactor red left autonomous routine; adjust movement parameters and delays for improved performance

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -39,26 +39,41 @@ void red_left_auton() {
 
     chassis.setPose(0, 0, 0);
     // used to be chassis.moveToPose(-5, 20, 0, 1000, {.forwards=true, .lead=0.4, .maxSpeed=65});
-    
-    chassis.moveToPose(-15, 18, 0, 1000, {.forwards=true, .lead=.3, .maxSpeed=60});
-    chassis.moveToPose(-20, 37, 0, 3000, {.forwards=true, .lead=.1, .maxSpeed=80});
+
+    //chassis.moveToPose(-5, 20, 0, 1000, {.forwards=true, .lead=.0, .maxSpeed=65});
+    intake(127);
     sort(127);
     outtake(-127);
     middleTake(127);
-    pros::delay(2000);
+    chassis.moveToPose(0, 21, 1, 500, {.forwards=true, .lead=0, .maxSpeed=50}); // prev x = -3 and lead = 0 and maxSpeed = 50
+    pros::delay(10);
+    chassis.moveToPose(0, 25, 1, 1000, {.forwards=true, .lead=0, .maxSpeed=50}); // prev x = -3 and lead = 0 and maxSpeed = 50
+   // pros::delay(250);
+    chassis.moveToPose(0, 30, 1, 1000, {.forwards=true, .lead=0, .maxSpeed=50});
+    middleTake(0);
+    chassis.moveToPose(0, 32, 69, 1000, {.forwards=true, .lead=0, .maxSpeed=50});
+ 
+   // pros::delay(250);
+    chassis.moveToPose(20, 36, 72, 1500, {.forwards=true, .lead=0, .maxSpeed=55}); //25x 41y
+    //chassis.moveToPose(18, 42, 94, 3000, {.forwards=true, .lead=.0, .maxSpeed=60});
+    pros::delay(1250);
+    intake(127);
+    sort(127);
     outtake(127);
-    sort(-127);
     middleTake(-127);
-    pros::delay(2000);
+    pros::delay(500);
+    
+    
 
     /*chassis.moveToPose(-25,4.5,135,3500, {.forwards=false, .lead=0, .maxSpeed=110});
-    matchLoad.set_value(1);
-    sort(127);
-    middleTake(127);
-    outtake(127);
-    chassis.moveToPose(-25,-10.5,0,2200, {.forwards=true, .lead=0, .maxSpeed=90});
-    pros::delay(2500);
-    chassis.moveToPose(-25,-24,0,2200, {.forwards=false, .lead=0, .maxSpeed=110});*/
+    pros::delay(2000);
+    //matchLoad.set_value(1);
+   //sort(127);
+    //middleTake(127);
+    //outtake(127);
+    //chassis.moveToPose(-25,-10.5,0,2200, {.forwards=true, .lead=0, .maxSpeed=90});
+    //pros::delay(2500);
+    //chassis.moveToPose(-25,-24,0,2200, {.forwards=false, .lead=0, .maxSpeed=110});*/
 
     
 
