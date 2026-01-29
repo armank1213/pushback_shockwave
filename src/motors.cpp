cc89c113f1d1c2276c86bb0cac30b6fd49d4b436
achintyank <achintyan.kaliraj@gmail.com>
2026-01-28 21:23:28 -0800

    Added passive outtake limiter

--- src/motors.cpp
+++ src/motors.cpp
@@ -28,52 +28,6 @@ void halfMotorControl() {
     halfMotor.move(-127);
 }
 
-void matchloadToggle() {
-
-    // Matchload piston variables
-    static bool matchloadPistonToggle = false;
-    static bool lastAButtonState = false;
-
-    // Matchload Pneumatics Toggle
-    bool currentA = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B);
-    if (currentA && !lastAButtonState) {
-        matchloadPistonToggle = !matchloadPistonToggle;
-        matchLoad.set_value(matchloadPistonToggle);
-    }
-    lastAButtonState = currentA;
-}
-void limiterToggle() {
-
-    // Limiter piston variables
-    static bool limiterPistonToggle = false;
-    static bool lastYButtonState = false;
-    
-    // Limiter Pneumatics Toggle
-    bool currentY = controller.get_digital(pros::E_CONTROLLER_DIGITAL_X);
-
-    if (currentY && !lastYButtonState) {
-        limiterPistonToggle = !limiterPistonToggle;
-        limiter.set_value(limiterPistonToggle);
-    }
-    lastYButtonState = currentY;
-}
-
-void wingToggle() {
-
-    static bool wingPistonToggle = false;
-    static bool lastXButtonState = false;
-
-    bool currentX = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
-        
-    if (currentX && !lastXButtonState) {
-        wingPistonToggle = !wingPistonToggle;
-        wing.set_value(wingPistonToggle);
-    }
-    
-    lastXButtonState = currentX;
-}
-
-
 // Control functions
 void intakeControl() {
     if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
@@ -83,7 +37,7 @@ void intakeControl() {
         intake(-127);
     }
     else {
-        intake(0);
+        intake(30);
     }
 }
 
@@ -95,7 +49,7 @@ void outtakeControl() {
         outtake(-127);
     }
     else {
-        outtake(0);
+        outtake(30);
     }
 }
 
